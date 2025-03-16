7c081a622041c13817c7dec90be94a367c3d53ef
Tommy Le <144294383+Tummy2@users.noreply.github.com>
2025-03-15 23:36:21 -0500

    finished two sum

---
+++ Array&Hashing/twoSum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        d = {}
+        for i in range(len(nums)):
+          num = nums[i]
+          need = target - num
+          if need in d:
+            return [d[need], i]
+          else:
+            d[num] = i
+
+test = Solution()
+
+nums = [3,4,5,6]
+target = 7
+# Output: [0,1]
+print(test.twoSum(nums, target))
+
+nums = [4,5,6]
+target = 10
+# Output: [0,2]
+print(test.twoSum(nums, target))
