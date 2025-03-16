d6ca711e4b5a46a85389b5066a5677fe4356b665
Tommy Le <144294383+Tummy2@users.noreply.github.com>
2025-03-16 00:40:11 -0500

    finished top k frequent elements

---
+++ Array&Hashing/topKFrequentElements.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        
+        d = defaultdict(int)
+        for i in range(len(nums)):
+            d[nums[i]] += 1
+
+        output = []
+        for i in range(k):
+            num = max(d, key=d.get)
+            output.append(num)
+            del d[num]
+
+        return output
+
+test = Solution()
+
+nums = [1,2,2,3,3,3]
+k = 2
+# Output: [2,3]
+print(test.topKFrequent(nums, k))
+
+nums = [7,7]
+k = 1
+# Output: [7]
+
+print(test.topKFrequent(nums, k))
+
+# Time: (O(N^2 k))
+# Space: (O(N))
