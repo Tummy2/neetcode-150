eed6be712addbf3badc9e1cb628a141550940939
Tommy Le <144294383+Tummy2@users.noreply.github.com>
2025-03-16 01:33:33 -0500

    finished encode and decode

--- Array&Hashing/encodeAndDecode.py
+++ Array&Hashing/encodeAndDecode.py
@@ -8,6 +8,31 @@ class Solution:
 
         return s.split("LMFAO")[0:-1]
     
+# THE GOOD SOLUTION
+# class Solution:
+    
+#     def encode(self, strs: List[str]) -> str:
+#         res = ""
+#         for s in strs:
+#             res += str(len(s)) + "#" + s
+#         return res
+
+#     def decode(self, s: str) -> List[str]:
+#         res = []
+#         i = 0
+        
+#         while i < len(s):
+#             j = i
+#             while s[j] != '#':
+#                 j += 1
+#             length = int(s[i:j])
+#             i = j + 1
+#             j = i + length
+#             res.append(s[i:j])
+#             i = j
+            
+#         return res
+    
 test = Solution()
 
 Input = ["neet","code","love","you"]
